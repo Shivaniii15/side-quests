216b6dfe6068e8243010011bec7f1d3a7e99aa66
Shivaniii15 <105461293+Shivaniii15@users.noreply.github.com>
2026-09-04 22:20:59 +1200

    learning regex

--- log-analyzer/analyzer.py
+++ log-analyzer/analyzer.py
@@ -1,4 +1,6 @@
-with open("Apache_2k.log", "r") as file:
-    for line in file:
-        if "[error]" in line:
-            print(line)
+import re
+
+line = "[Sun Dec 04 04:52:15 2005] [error] mod_jk child workerEnv in error state 7"
+
+match = re.search(r"\[(.*?)\]", line)
+print(match.group(1))
