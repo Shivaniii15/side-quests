11db006745e01685cb6e80e2f6d3ff7aaea7b662
Shivaniii15 <105461293+Shivaniii15@users.noreply.github.com>
2026-09-04 22:41:55 +1200

    using hourkey and strftime to print

--- log-analyzer/analyzer.py
+++ log-analyzer/analyzer.py
@@ -1,8 +1,19 @@
+from datetime import datetime
 import re
+
+hourly_counts = {}
 
 with open("Apache_2k.log", "r") as file:
     for line in file:
         if "[error]" in line:
             match = re.search(r"\[(.*?)\]", line)
             timestamp = match.group(1)
-            print(timestamp)
+            dt = datetime.strptime(timestamp, "%a %b %d %H:%M:%S %Y")
+            hour_key = dt.strftime("%Y-%m-%d %H:00")
+
+            if hour_key in hourly_counts:
+                hourly_counts[hour_key] += 1
+            else:
+                hourly_counts[hour_key] = 1
+
+print(hourly_counts)
